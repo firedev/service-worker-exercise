37f043c61390627b13db18ebf4ac1caccc00c943
Nick Ostrovsky <nick@firedev.com>
2020-11-23 17:16:45 +0300

    online indicator

--- web/js/blog.js
+++ web/js/blog.js
@@ -1,14 +1,26 @@
-;(function Blog() {
+;
+(function Blog() {
   'use strict'
 
   var offlineIcon
   var isLoggedIn = /isLoggedIn=1/.test(document.cookie.toString() || '')
+  var isOnline = 'onLine' in navigator ? navigator.onLine : true
 
-  document.addEventListener('DOMContentLoaded', ready, false)
-
-  // **********************************
+  document.addEventListener("DOMContentLoaded", ready, false)
 
   function ready() {
     offlineIcon = document.getElementById('connectivity-status')
+    updateIcon(isOnline)
+    window.addEventListener('online', () => {
+      updateIcon(true)
+    })
+    window.addEventListener('offline', () => {
+      updateIcon(false)
+    })
+  }
+
+  function updateIcon(status) {
+    isOnline = status
+    offlineIcon.style.display = isOnline ? 'none' : 'block'
   }
 })()
