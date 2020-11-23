ac4cba63eff4e6d2975e1eeb52bc7da3b8e739a0
Nick Ostrovsky <nick@firedev.com>
2020-11-23 18:39:26 +0300

    Service Worker

--- web/js/sw.js
+++ web/js/sw.js
@@ -1,4 +1,26 @@
 'use strict'
 
-// TODO
+const version = 1;
 
+self.addEventListener('install', onInstall)
+self.addEventListener('activate', onActivate)
+
+main().catch(console.error)
+
+async function main() {
+  console.log(`worker v${version} is starting`)
+}
+
+async function onInstall(event) {
+  console.log(`worker v${version} is installed`)
+  self.skipWaiting()
+}
+
+async function onActivate(event) {
+  event.waitUntil(handleActivation())
+}
+
+async function handleActivation() {
+  await clients.claim()
+  console.log(`worker v${version} is activated`)
+}
