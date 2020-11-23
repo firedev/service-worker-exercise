1d12a0392f7b2d3705fa58298914fcd0bfb6993d
Nick Ostrovsky <nick@firedev.com>
2020-11-23 17:11:01 +0300

    Project Structure

---
+++ web/js/blog.js
@@ -0,0 +1,14 @@
+;(function Blog() {
+  'use strict'
+
+  var offlineIcon
+  var isLoggedIn = /isLoggedIn=1/.test(document.cookie.toString() || '')
+
+  document.addEventListener('DOMContentLoaded', ready, false)
+
+  // **********************************
+
+  function ready() {
+    offlineIcon = document.getElementById('connectivity-status')
+  }
+})()
