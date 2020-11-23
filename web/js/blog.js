e0624de61144a2376cfb9c519c3d6aaeb7ebf140
Nick Ostrovsky <nick@firedev.com>
2020-11-23 17:22:07 +0300

    Register ServiceWorker

--- web/js/blog.js
+++ web/js/blog.js
@@ -6,7 +6,26 @@
   var isLoggedIn = /isLoggedIn=1/.test(document.cookie.toString() || '')
   var isOnline = 'onLine' in navigator ? navigator.onLine : true
 
+
+  var usingSW = ("serviceWorker" in navigator)
+  var swRegistration;
+  var serviceWorker;
+
+  initServiceWorker().catch(console.error)
+
   document.addEventListener("DOMContentLoaded", ready, false)
+
+  async function initServiceWorker() {
+    swRegistration = await navigator.serviceWorker.register("/sw.js", {
+      updateViaCache: "none",
+    })
+
+    serviceWorker = swRegistration.installing || swRegistration.waiting || swRegistration.active
+
+    navigator.serviceWorker.addEventListener("controllerchange", function onControllerChange() {
+      serviceWorker = navigator.serviceWorker.controller
+    })
+  }
 
   function ready() {
     offlineIcon = document.getElementById('connectivity-status')
