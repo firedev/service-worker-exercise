8f9423331ed3b11ad3f561b4156f0ff866cc45a8
Nick Ostrovsky <nick@firedev.com>
2020-11-25 10:13:56 +0300

    Add force-logout for blog.js and lint it

--- web/js/blog.js
+++ web/js/blog.js
@@ -1,52 +1,11 @@
-;
 (function Blog() {
-  'use strict'
-
   let offlineIcon
   let isLoggedIn = /isLoggedIn=1/.test(document.cookie.toString() || '')
   let isOnline = 'onLine' in navigator ? navigator.onLine : true
 
-
-  let usingSW = ('serviceWorker' in navigator)
+  // const usingSW = ('serviceWorker' in navigator)
   let swRegistration;
   let serviceWorker;
-
-  initServiceWorker().catch(console.error)
-
-  document.addEventListener("DOMContentLoaded", ready, false)
-
-  async function initServiceWorker() {
-    swRegistration = await navigator.serviceWorker.register("/sw.js", {
-      updateViaCache: "none",
-    })
-
-    serviceWorker = swRegistration.installing || swRegistration.waiting || swRegistration.active
-    sendStatusUpdate(serviceWorker)
-
-    navigator.serviceWorker.addEventListener('controllerchange', function onControllerChange() {
-      serviceWorker = navigator.serviceWorker.controller
-      sendStatusUpdate(serviceWorker)
-    })
-    navigator.serviceWorker.addEventListener('message', onSWMessage)
-  }
-
-  function onSWMessage(event) {
-    const {
-      data
-    } = event
-    if (data.requestStatusUpdate) {
-      sendStatusUpdate(event.ports && event.ports[0])
-    }
-  }
-
-  function sendStatusUpdate(target) {
-    sendSWMessage({
-      statusUpdate: {
-        isOnline,
-        isLoggedIn
-      }
-    }, target)
-  }
 
   function sendSWMessage(msg, target) {
     if (target) {
@@ -56,7 +15,56 @@
     } else if (navigator.serviceWorker.controller) {
       navigator.serviceWorker.controller.postMessage(msg)
     } else {
+      // eslint-disable-next-line no-console
       console.warn(msg, 'is discarded')
+    }
+  }
+
+  function sendStatusUpdate(target) {
+    sendSWMessage({
+      statusUpdate: {
+        isOnline,
+        isLoggedIn,
+      },
+    }, target)
+  }
+
+  function onSWMessage(event) {
+    const {
+      data,
+    } = event
+    if (data.requestStatusUpdate) {
+      sendStatusUpdate(event.ports && event.ports[0])
+    } else if (data === 'force-logout') {
+      document.cookie = 'isLoggedIn='
+      isLoggedIn = false
+      sendStatusUpdate()
+    }
+  }
+
+  async function initServiceWorker() {
+    swRegistration = await navigator.serviceWorker.register('/sw.js', {
+      updateViaCache: 'none',
+    })
+
+    serviceWorker = swRegistration.installing || swRegistration.waiting || swRegistration.active
+    sendStatusUpdate(serviceWorker)
+
+    navigator.serviceWorker.addEventListener('controllerchange', () => {
+      serviceWorker = navigator.serviceWorker.controller
+      sendStatusUpdate(serviceWorker)
+    })
+    navigator.serviceWorker.addEventListener('message', onSWMessage)
+  }
+
+  function updateIcon(status) {
+    isOnline = status
+    if (isOnline) {
+      offlineIcon.classList.add('hidden')
+      sendStatusUpdate(serviceWorker)
+    } else {
+      offlineIcon.classList.remove('hidden')
+      sendStatusUpdate(serviceWorker)
     }
   }
 
@@ -72,14 +80,7 @@
     sendStatusUpdate()
   }
 
-  function updateIcon(status) {
-    isOnline = status
-    if (isOnline) {
-      offlineIcon.classList.add("hidden")
-      sendStatusUpdate(serviceWorker)
-    } else {
-      offlineIcon.classList.remove("hidden")
-      sendStatusUpdate(serviceWorker)
-    }
-  }
-})()
+  // eslint-disable-next-line no-console
+  initServiceWorker().catch(console.error)
+  document.addEventListener('DOMContentLoaded', ready, false)
+}())
